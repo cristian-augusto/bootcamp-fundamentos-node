e1f4aa924dbd35475dd072177c0aa958f09cadb3
Cristian Augusto <cristianaugusto2013@gmail.com>
2020-07-11 12:54:04 -0300

    Conclusao desafio bootcamp

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,14 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +16,21 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: Request): Transaction {
+    if (type !== 'income' && type !== 'outcome')
+      throw new Error('The type must be income or outcome');
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value)
+      throw new Error('Not enought balance for this transaction.');
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+    return transaction;
   }
 }
 
